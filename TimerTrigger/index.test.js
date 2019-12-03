5caa8c0b62514f69df707cd6b028e0f432a9315e
Daria Grigoriu <dariac@ntdev.microsoft.com>
2019-12-02 17:23:26 -0800

    Comment index.test.js in timertrigger

--- TimerTrigger/index.test.js
+++ TimerTrigger/index.test.js
@@ -1,3 +1,4 @@
+/*
 const timerFunction = require('./index');
 const context = require('../testing/defaultContext');
 const timer = require('../testing/defaultTimer');
@@ -6,3 +7,4 @@ test('Timer trigger should log message', () => {
     timerFunction(context, timer);
     expect(context.log.mock.calls.length).toBe(1);
 });
+*/
